1f53e11ff7142887fe52a3d6dae6330a1d55a9f7
Kazuhiro Serizawa <nserihiro@gmail.com>
2018-07-25 12:13:31 +0900

    update generate_jmagpz.py

--- generate_jmagpz.py
+++ generate_jmagpz.py
@@ -14,18 +14,21 @@ from PIL import Image
 def generate():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', '-g', type=int, default=-1)
-    parser.add_argument('--model', '-m', type=str, default=None)
-    parser.add_argument('--id', '-i', type=int, default=0)
+    parser.add_argument('--model', '-m', type=str, required=True)
+    parser.add_argument('--id', '-i', type=int, required=True)
     parser.add_argument('--inf', type=int, default=10)
     parser.add_argument('--outf', type=int, default=10)
+    parser.add_argument('--test_data_index', '-test_data', nargs='+', default=[4000, 5225])
+    parser.add_argument('--files', '-f', nargs='+', default=['jmagpz.npy'])
     args = parser.parse_args()
 
-    test = dataset.JmaGpzDataset(0, 5225, args.inf, args.outf)
+    test = dataset.JmaGpzDataset(args.test_data_index[0], args.test_data_index[1],
+                                 args.inf, args.outf, file=args.files)
+
     model = network.JmaGpzNetwork(sz=[128, 64, 64], n=1, directory="img/")
 
-    if args.model != None:
-        print( "loading model from " + args.model )
-        serializers.load_npz(args.model, model)
+    print( "loading model from " + args.model )
+    serializers.load_npz(args.model, model)
 
     x, t = test[args.id]
 
