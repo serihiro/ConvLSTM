49ec3dc6ec0add3d8d7b377d85a7f4d9cb9e142b
Kazuhiro Serizawa <nserihiro@gmail.com>
2019-03-21 11:37:22 +0900

    implement a DataSet for distributed jmagpv files

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 import numpy as np
 import chainer
 
+
 class MovingMnistDataset(chainer.dataset.DatasetMixin):
     def __init__(self, l, r, inn, outn, path="./mnist_test_seq.npy"):
         self.l = l
@@ -10,10 +11,31 @@ class MovingMnistDataset(chainer.dataset.DatasetMixin):
         self.data = np.load(path)
         self.data[self.data < 128] = 0
         self.data[self.data >= 128] = 1
-        
+
     def __len__(self):
         return self.r - self.l
 
     def get_example(self, i):
         ind = self.l + i
-        return self.data[:self.inn, ind, :, :].astype(np.int32), self.data[self.inn:self.inn+self.outn, ind, :, :].astype(np.int32)
+        return self.data[:self.inn, ind, :, :].astype(np.int32), self.data[self.inn:self.inn + self.outn, ind, :,
+                                                                 :].astype(np.int32)
+
+
+class JmaGpvDataset(chainer.dataset.DatasetMixin):
+    def __init__(self, index_file_path, n_in, n_out, threshold=5.0):
+        with open(index_file_path, mode='r') as f:
+            self._path_list = f.read().split('\n')
+        self._n_in = n_in
+        self._n_out = n_out
+        self._threshold = threshold
+
+    def __len__(self):
+        return len(self._path_list)
+
+    def get_example(self, i):
+        data = np.load(self._path_list[i])
+        data[data < self._threshold] = 0
+        data[data >= self._threshold] = 1
+
+        return data[:self._n_in, :, :, :].astype(np.int32), data[self._n_in:self._n_in + self._n_out, :, :, :].astype(
+            np.int32)
